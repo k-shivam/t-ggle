51f74ef497de4bf12cae5b793aed52f57cd803ea
k-shivam <42085548+k-shivam@users.noreply.github.com>
2025-01-05 21:16:04 +0530

    Create constants.js

---
+++ src/components/constants.js
@@ -0,0 +1,12 @@
+const platformImages = {
+  Zepto: "https://qcsearch.s3.ap-south-1.amazonaws.com/zepto.png",
+  BlinkIt: "https://qcsearch.s3.ap-south-1.amazonaws.com/blinkit.png",
+  Swiggy: "https://qcsearch.s3.ap-south-1.amazonaws.com/swiggy.png",
+};
+
+const platformUrlMapping = {
+  Zepto: `https://www.zeptonow.com/pn`,
+  BlinkIt: `https://blinkit.com/prn`,
+  Swiggy: `https://www.swiggy.com/instamart/item`,
+};
+export default platformImages;
